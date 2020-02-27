912ddfeb6ec9c4708c5314aff6d0d24f849a6070
hanhle <hanh.lethi@gameloft.com>
2020-02-27 11:00:15 +0700

    update

--- PythonWeb/blog/tests.py
+++ PythonWeb/blog/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
-
+from .models import Post
 # Create your tests here.
+class BlogTest(TestCase):
+    def setUp(seft):
+        Post.objects.create(
+            title='myTitle',
+            body='just a Test'
+        )
+    def test_string_representation(self):
+        post = Post(title='My entry title')
+        self.assertEqual(str(post), post.title)
+    def test_post_list_view(self):
+        respone = self.client.get('/blog/')
+        self.assertEqual(respone.status_code, 200)
+        self.assertContains(respone,'myTitle')
+        self.assertTemplatesUsed(respone, 'blog/blog.html')
+    def test_post_detail_views(self):
+        respone = self.client.get('/blog/1/')
+        self.assertEqual(respone.status_code, 200)
+        self.assertContains(respone,'myTitle')
+        self.assertTemplatesUsed(respone, 'blog/post.html')
